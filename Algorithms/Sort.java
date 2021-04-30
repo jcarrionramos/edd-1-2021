61aa5b069f044c255f0ff85c93b443d1d4d17f1c
jcarrionramos <javiercarrionramos@gmail.com>
2021-04-30 17:14:55 -0400

    bst

--- Algorithms/Sort.java
+++ Algorithms/Sort.java
@@ -50,4 +50,59 @@ public class Sort {
       arr[j + 1] = swapValue;
     }
   }
+
+  public static void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int leftArray[] = new int[n1];
+    int rightArray[] = new int[n2];
+
+    for (int i = 0; i < n1; i++) {
+      leftArray[i] = arr[i + left];
+    }
+
+    for (int j = 0; j < n2; j++) {
+      rightArray[j] = arr[j + (mid + 1)];
+    }
+
+    int i = 0, j = 0;
+    int k = left;
+    while (i < n1 && j < n2) {
+      if (leftArray[i] <= rightArray[j]) {
+        arr[k] = leftArray[i];
+        i++;
+      } else {
+        arr[k] = rightArray[j];
+        j++;
+      }
+      k++;
+    }
+
+    while (i < n1) {
+      arr[k] = leftArray[i];
+      i++;
+      k++;
+    }
+
+    while (j < n2) {
+      arr[k] = rightArray[j];
+      j++;
+      k++;
+    }
+  }
+
+  public static void MergeSort(int arr[], int left, int right) {
+    if (left < right) {
+      int mid = (left + right) / 2;
+
+      // Desordenado
+      MergeSort(arr, left, mid);
+      MergeSort(arr, mid + 1, right);
+
+      // Ordenados
+      merge(arr, left, mid, right);
+    }
+  }
+
 }
---
+++ Structures/TreeNode.java
@@ -0,0 +1,12 @@
+package Structures;
+
+public class TreeNode {
+  public int value;
+  public TreeNode left;
+  public TreeNode right;
+
+  public TreeNode(int value) {
+    this.value = value;
+    left = right = null;
+  }
+}
